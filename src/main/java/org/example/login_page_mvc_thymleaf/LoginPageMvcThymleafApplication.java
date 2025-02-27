cec40de9d8975d346178eed6cf28d67655ea3171
Azazagich <s.kovalchuk@innovinnprom.com>
2025-02-27 20:18:27 +0200

    first commit

---
+++ src/main/java/org/example/login_page_mvc_thymleaf/LoginPageMvcThymleafApplication.java
@@ -0,0 +1,13 @@
+package org.example.login_page_mvc_thymleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginPageMvcThymleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginPageMvcThymleafApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/login_page_mvc_thymleaf/LoginPageMvcThymleafApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.login_page_mvc_thymleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginPageMvcThymleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
